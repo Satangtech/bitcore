0be22c96de7abe8582b788e40065764513215e61
Ic3Sandy <monkey_king2@hotmail.com>
2022-05-20 15:33:19 +0700

    🚧 Add api for search bar

---
+++ packages/bitcore-node/src/modules/firocoin/api/search.ts
@@ -0,0 +1,104 @@
+import express = require('express');
+import { TransactionStorage } from '../../../models/transaction';
+import { ContractStorage } from '../models/contract';
+import { TokenStorage } from '../models/token';
+const router = express.Router({ mergeParams: true });
+
+router.get('/:content', async (req, res) => {
+  let { chain, network, content } = req.params;
+  try {
+    content = Buffer.from(content, 'base64').toString('utf-8');
+
+    // TODO: Other Address
+    // const regexBitcoinAddresses = /([13]|bc1)[A-HJ-NP-Za-km-z1-9]{27,34}/g;
+
+    const regexNativeAddress = /T[A-HJ-NP-Za-km-z1-9]{33}/g;
+    const matchNativeAddress = content.match(regexNativeAddress);
+    if (matchNativeAddress && content.length === 33) {
+      const nativeAddress = matchNativeAddress[0];
+      res.json({
+        type: 'account',
+        native: nativeAddress,
+      });
+      return;
+    }
+
+    const regexHexAddress = /[0-9a-fA-F]{40}/g;
+    const matchHexAddress = content.match(regexHexAddress);
+    if (matchHexAddress && content.length === 40) {
+      const hexAddress = matchHexAddress[0];
+      const contract = await ContractStorage.collection.findOne({ chain, network, contractAddress: hexAddress });
+      if (contract) {
+        res.json({
+          type: 'contract',
+          address: hexAddress,
+        });
+      } else {
+        res.json({
+          type: 'account',
+          hex: hexAddress,
+        });
+      }
+      return;
+    }
+
+    const regexBlockNumber = /[0-9]{1,15}/g;
+    const matchBlockNumber = content.match(regexBlockNumber);
+    if (matchBlockNumber && content.length <= 15) {
+      const blockId = matchBlockNumber[0];
+      res.json({
+        type: 'blocknumber',
+        blockId,
+      });
+      return;
+    }
+
+    const regexHash = /[0-9a-zA-Z]{64}/g;
+    const matchHash = content.match(regexHash);
+    if (matchHash && content.length === 64) {
+      const hash = matchHash[0];
+      const transaction = await TransactionStorage.collection.findOne({ chain, network, txid: hash });
+      if (transaction) {
+        res.json({
+          type: 'txid',
+        });
+      } else {
+        res.json({
+          type: 'blockid',
+        });
+      }
+      return;
+    }
+
+    const tokens = await TokenStorage.collection
+      .find({
+        chain,
+        network,
+        $or: [{ symbol: new RegExp(content, 'i') }, { name: new RegExp(content, 'i') }],
+      })
+      .toArray();
+    if (tokens.length > 0) {
+      const result: any = [];
+      for (let token of tokens) {
+        result.push({
+          address: token.contractAddress,
+          name: token.name,
+          symbol: token.symbol,
+        });
+      }
+      res.json({
+        type: 'token',
+        result,
+      });
+    } else {
+      res.status(404).send(`Search ${content} could not be found.`);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = {
+  router,
+  path: '/search',
+};
