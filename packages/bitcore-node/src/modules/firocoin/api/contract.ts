362aa7b6a81e8b9662d0c05288b8671ca33fc5f0
Ic3Sandy <monkey_king2@hotmail.com>
2022-06-07 21:56:33 +0700

    🚧 Modify for return err out

--- packages/bitcore-node/src/modules/firocoin/api/contract.ts
+++ packages/bitcore-node/src/modules/firocoin/api/contract.ts
@@ -193,12 +193,12 @@ router.post('/:contractAddress', upload.single('file'), async (req, res) => {
         res.status(400).send('Verify Fail!');
       } catch (err) {
         console.error(err);
-        res.status(500).send('Something went wrong!');
+        res.status(500).send(err);
       }
     });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Something went wrong!');
+    res.status(500).send(err);
   }
 });
 
