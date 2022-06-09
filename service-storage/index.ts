2f71cab605712877baf176f0bc4335be8fc9aca6
Ic3Sandy <monkey_king2@hotmail.com>
2022-06-09 11:58:47 +0700

    🚧 Add service redis

--- service-storage/index.ts
+++ service-storage/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import * as fs from 'fs';
 import basicAuth from 'express-basic-auth';
 import { folderUpload, GGStorage } from './storage';
+import { getKeys, getValue, setValue } from './redis';
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -22,13 +23,24 @@ app.get('/contracts/:contractAddress', async (req: Request, res: Response) => {
     .access(`${folderUpload}/${contractAddress}.json`, fs.constants.F_OK)
     .then(() => true)
     .catch(() => false);
+  let jsonObj = {};
 
-  if (!fileExists) {
+  if (fileExists) {
+    jsonObj = JSON.parse(await fs.promises.readFile(`${folderUpload}/${contractAddress}.json`, 'utf8'));
+    res.send(jsonObj);
+    return;
+  }
+
+  const value = await getValue(contractAddress);
+  if (value) {
+    await fs.promises.writeFile(`${folderUpload}/${contractAddress}.json`, value, 'utf8');
+    jsonObj = JSON.parse(value);
+  } else {
     const ggStorage = new GGStorage();
     await ggStorage.downloadFile(contractAddress);
+    jsonObj = JSON.parse(await fs.promises.readFile(`${folderUpload}/${contractAddress}.json`, 'utf8'));
+    await setValue(contractAddress, JSON.stringify(jsonObj));
   }
-  console.log('fileExists', fileExists);
-  const jsonObj = JSON.parse(await fs.promises.readFile(`${folderUpload}/${contractAddress}.json`, 'utf8'));
   res.send(jsonObj);
 });
 
@@ -44,7 +56,25 @@ app.post('/contracts/:contractAddress', async (req: Request, res: Response) => {
   await fs.promises.writeFile(`${folderUpload}/${contractAddress}.json`, JSON.stringify(jsonObj), 'utf8');
   const ggStorage = new GGStorage();
   await ggStorage.uploadFile(contractAddress);
-  res.sendStatus(200);
+  await setValue(contractAddress, JSON.stringify(jsonObj));
+  res.sendStatus(201);
+});
+
+app.get('/key/:key', async (req: Request, res: Response) => {
+  const { key } = req.params;
+  const result = await getValue(key);
+  res.send(result);
+});
+
+app.post('/key/:key', async (req: Request, res: Response) => {
+  const { key } = req.params;
+  await setValue(key, JSON.stringify(req.body));
+  res.sendStatus(201);
+});
+
+app.get('/keys', async (req: Request, res: Response) => {
+  const result = await getKeys();
+  res.send(result);
 });
 
 app.listen(port, () => {
---
+++ service-storage/redis.ts
@@ -0,0 +1,27 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+  password: process.env.REDIS_PASSWORD,
+});
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+export const getValue = async (key: string) => {
+  await client.connect();
+  const value = await client.get(key);
+  await client.disconnect();
+  return value;
+};
+
+export const setValue = async (key: string, value: string) => {
+  await client.connect();
+  await client.set(key, value, { EX: 60 * 60 * 24 }); // 24 hr expire
+  await client.disconnect();
+};
+
+export const getKeys = async () => {
+  await client.connect();
+  const result = await client.keys('*');
+  await client.disconnect();
+  return result;
+};
