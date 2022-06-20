9fe2d9eb3907c0c264a326900c9da8b667629c33
Ic3Sandy <monkey_king2@hotmail.com>
2022-06-20 15:09:09 +0700

    🚧 Add cache for api stats

--- packages/bitcore-node/src/modules/firocoin/api/stats.ts
+++ packages/bitcore-node/src/modules/firocoin/api/stats.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import { BitcoinBlockStorage } from '../../../models/block';
+import { CacheStorage } from '../../../models/cache';
 import { TransactionStorage } from '../../../models/transaction';
 import { WalletAddressStorage } from '../../../models/walletAddress';
 import { resMessage } from '../utils';
@@ -7,6 +8,10 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/', async (_, res) => {
   try {
+    const cache = await CacheStorage.getGlobal('stats');
+    if (cache) {
+      return res.json(cache);
+    }
     const totalTxns = await TransactionStorage.collection.countDocuments();
     const totalBlocks = await BitcoinBlockStorage.collection.countDocuments();
     const walletAddress = await WalletAddressStorage.collection.countDocuments();
@@ -22,10 +27,11 @@ router.get('/', async (_, res) => {
       total_blocks: totalBlocks,
       wallet_address: walletAddress,
     };
-    res.json(result);
+    await CacheStorage.setGlobal('stats', result, 15 * CacheStorage.Times.Second);
+    return res.json(result);
   } catch (err) {
     console.error(err);
-    res.status(500).send(resMessage((<any>err).message));
+    return res.status(500).send(resMessage((<any>err).message));
   }
 });
 
