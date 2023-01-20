fd5fce4c20fdadb831c4c41c0c659bb65c1228b3
Ic3Sandy <monkey_king2@hotmail.com>
2023-01-20 17:50:49 +0700

    🚧 Update code for auth mongo

--- timeseries/createCollection.js
+++ timeseries/createCollection.js
@@ -1,9 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = `mongodb://${process.env.DB_HOST}:27017/`;
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_HOST = process.env.DB_HOST || 'db';
+const DB_NAME = process.env.DB_NAME || 'bitcore';
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const url = DB_USER ? `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}` : `mongodb://${DB_HOST}:${DB_PORT}`;
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  const dbo = db.db(process.env.DB_NAME);
+  const dbo = db.db(DB_NAME);
   const collection = 'txns';
   dbo.createCollection(
     collection,
@@ -27,7 +32,7 @@ MongoClient.connect(url, function (err, db) {
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  const dbo = db.db(process.env.DB_NAME);
+  const dbo = db.db(DB_NAME);
   const collection = 'gas';
   dbo.createCollection(
     collection,
